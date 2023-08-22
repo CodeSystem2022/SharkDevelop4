838323d0a74767aaf9b2d2ac56a6dbf7b40d1a37
LucasSGallo <109878265+LucasSGallo@users.noreply.github.com>
2023-08-22 20:18:28 -0300

    JS primera clase

---
+++ JavaScript leccion 01/ecommerce/client/media/products.js
@@ -0,0 +1,39 @@
+
+const productos = [
+    {
+        id: 1,
+        productName: "Banana",
+        price: 400,
+        quanty: 1,
+        img: 'banana.png'
+    },
+    {
+        id: 2,
+        productName: "Leche",
+        price: 950,
+        quanty: 1,
+        img: 'leche.png'
+    },
+    {
+        id: 3,
+        productName: "Pollo",
+        price: 1300,
+        quanty: 1,
+        img: 'pollo.png'
+    },
+    {
+        id: 4,
+        productName: "Mayonesa",
+        price: 1000,
+        quanty: 5,
+        img: 'mayonesa.png'
+    },
+    {
+        id: 5,
+        productName: "Envasados",
+        price: 2500,
+        quanty: 12,
+        img: 'envasados.png'
+    }
+                    ]
+
