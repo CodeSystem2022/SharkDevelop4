0bd9040b8c32925235d025475408e75898623285
José Carmona <92607774+PepCarm@users.noreply.github.com>
2023-10-24 23:25:08 -0300

    Update file Node

---
+++ Java/Node/01-Hola_mundo/monohilo.js
@@ -0,0 +1,14 @@
+console.log('Hola a toda la cohorte 2022');
+
+var i = 0;
+setInterval(function() {
+    console.log(i);//Crtl+c para detener la ejecución
+    i++;
+
+    // if(i === 5){
+    //     console.log('Forzamos un error');
+    //     var a = 3 + z;
+    // }
+}, 1000);//Milisegundos
+console.log('Segunda instrucción');
+
