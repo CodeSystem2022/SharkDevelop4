0563eecc4cef40475e19a9e90fc1fe7e3d9d785b
José Carmona <92607774+PepCarm@users.noreply.github.com>
2023-10-24 23:23:23 -0300

    Archivos de Node

---
+++ Java/Node/01-Hola_mundo/app.js
@@ -0,0 +1,7 @@
+let nombre ='José';
+console.log(nombre);
+
+const nombre2 = "Juan";
+console.log(nombre2);
+
+console.log("Hola Mundo desde NodeJS");
---
+++ Java/Node/01-Hola_mundo/app2.js
@@ -0,0 +1,6 @@
+// Creamos una función asignada a ua constante
+const saludar = (nombre) => {  //Función de flechas (usar el acento frances)
+    return `Hola ${nombre}`; //esto es igual a 'Saludos'+nombre
+};
+
+console.log(saludar("Sharkdevelop"));
