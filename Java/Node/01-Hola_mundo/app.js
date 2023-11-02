9ebc28b50829e09dfa30c9b6edf2b4c18fd49c76
LucasSGallo <109878265+LucasSGallo@users.noreply.github.com>
2023-11-02 18:11:55 -0300

    Si, está, pero que se vea lindo

--- Java/Node/01-Hola_mundo/app.js
+++
@@ -1,7 +0,0 @@
-let nombre ='José';
-console.log(nombre);
-
-const nombre2 = "Juan";
-console.log(nombre2);
-
-console.log("Hola Mundo desde NodeJS");
--- Java/Node/01-Hola_mundo/app2.js
+++
@@ -1,6 +0,0 @@
-// Creamos una función asignada a ua constante
-const saludar = (nombre) => {  //Función de flechas (usar el acento frances)
-    return `Hola ${nombre}`; //esto es igual a 'Saludos'+nombre
-};
-
-console.log(saludar("Sharkdevelop"));
