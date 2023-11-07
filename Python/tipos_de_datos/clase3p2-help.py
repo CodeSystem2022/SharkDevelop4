09cf4377ea5e3dc330b0ca0d993c8471c7cc2203
Gabriel Valdes <104102901+bkt93@users.noreply.github.com>
2023-11-07 20:37:17 -0300

    clase3p2-help1y2

---
+++ Python/tipos_de_datos/clase3p2-help.py
@@ -0,0 +1,33 @@
+#clase 3 parte 2
+
+valor = 0
+resultado = bool(valor)
+print(f'valor: {valor}, Resultado: {resultado}')
+
+valor = 0.1
+resultado = bool(valor)
+print(f'valor: {valor}, Resultado: {resultado}')
+
+valor = ''
+resultado = bool(valor)
+print(f'valor: {valor}, Resultado: {resultado}')
+
+valor = 'Hola mundo'
+resultado = bool(valor)
+print(f'valor: {valor}, Resultado: {resultado}')
+
+
+
+# help() parte 1 y 2
+variable = 'adios'
+mensaje = 'Hola' 'Alumnos' + variable
+mensaje += ', Teminamos '
+
+print(mensaje)
+
+
+help(str)
+
+help(str.capitalize)
+
+help(str.isnan)
