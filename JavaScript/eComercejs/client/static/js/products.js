cc9b949b4bb22832b470be98fac1d7b89ce1cf5c
Gabriel Valdes <104102901+bkt93@users.noreply.github.com>
2023-11-07 20:40:52 -0300

    html-css-js commit

---
+++ JavaScript/eComercejs/client/static/js/products.js
@@ -0,0 +1,31 @@
+const productos = [
+
+    {
+        id: 1,
+        productName: "Remera",
+        price: 8500,
+        quanty: 1,
+        img: "static/media/remera.png",
+    },
+    {
+        id: 2,
+        productName: "Buzo",
+        price: 11000,
+        quanty: 1,
+        img: "static/media/buzo.png",
+    },
+    {
+        id: 3,
+        productName: "Pantalón",
+        price: 9000,
+        quanty: 1,
+        img: "static/media/pantalon.png",
+    },
+    {
+        id: 4,
+        productName: "Zapatillas",
+        price: 35000,
+        quanty: 1,
+        img: "static/media/zapatillas.png",
+    },
+];
